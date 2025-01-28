d7b81626fd3816b63efaa692d58e44c8d9d2c5d7
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-01-28 14:11:31 +0530

    adding prisma client

---
+++ lib/utils.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client"
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+
+const prisma = new PrismaClient()
+export default prisma
