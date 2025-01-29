d08a15bdd348ade4676a500c0b00f0be5a8363ec
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-01-29 11:15:19 +0530

    adding middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
    <div className="flex justify-center items-center mt-12">
   <Button>let's build formigo</Button>
+  
    </div>
   );
 }
