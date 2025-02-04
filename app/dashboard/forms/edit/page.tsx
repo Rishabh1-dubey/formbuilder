47c572c051fc9bf8be5e6e48c00f290429daa4cb
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-02-04 21:59:39 +0530

    gave ai power to our app

---
+++ app/dashboard/forms/edit/page.tsx
@@ -0,0 +1,9 @@
+
+
+const page = () => {
+  return (
+    <div>this the edit page</div>
+  )
+}
+
+export default page
---
+++ app/dashboard/forms/page.tsx
@@ -0,0 +1,8 @@
+
+const page = () => {
+  return (
+    <div>My Forms</div>
+  )
+}
+
+export default page
