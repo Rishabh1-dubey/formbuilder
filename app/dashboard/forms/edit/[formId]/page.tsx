dae65de643a3908383be340230252da2757849d0
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-02-05 13:33:47 +0530

    adding form id

---
+++ app/dashboard/forms/edit/[formId]/page.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Analtytics from "@/components/Analtytics";
+import prisma from "@/lib/prisma";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+const editPage = async ({
+  params,
+}: {
+  params: Promise<{ formId: string }>;
+}) => {
+  const formId = (await params).formId;
+  if (!formId) {
+    return <h1>NO form Id found for id {formId}</h1>;
+  }
+
+  const form: any = await prisma.form.findUnique({
+    where: {
+      id: Number(formId),
+    },
+  });
+  // console.log(form);
+  return (
+    <div className="grid grid-cols-[200px_minmax(900px,_1fr)_100px] ">
+      <Analtytics />
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            <h1 className="text-2xl text-center">{form.content?.title}</h1>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          {/* <AiGenratedform form ={form} isEditMode={true}/> */}
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default editPage;
--- app/dashboard/forms/edit/page.tsx
+++ app/dashboard/forms/edit/page.tsx
@@ -1,9 +1,23 @@
-
+"use client"
+import Analtytics from "@/components/Analtytics";
+import { useRouter } from "next/navigation";
 
 const page = () => {
+  const router = useRouter()
+  const handleClick=()=>{
+    router.push('/dashboard/analytics');
+  }
   return (
-    <div>this the edit page</div>
-  )
-}
+    <div>
+      <div className="grid grid-cols-[200px_minmax(900px,_1fr)_100px] ">
+        <div onClick={handleClick}>
+        <Analtytics />
 
-export default page
+        </div>
+        <div className="py-2 px-4">from sectionn for editong</div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
