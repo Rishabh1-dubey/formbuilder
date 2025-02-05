3c49015c30c908228ba36cd9be02ff2d76b5a8e9
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-02-05 13:34:23 +0530

    adding anltytics page

---
+++ app/dashboard/analytics/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import Analtytics from "@/components/Analtytics";
+import { useRouter } from "next/navigation";
+const page = () => {
+ 
+  return (
+    <div>
+    <div className="grid grid-cols-[200px_minmax(900px,_1fr)_100px] ">
+     
+     <div>
+      <Analtytics />
+      </div>
+    <div className="py-2 px-4">edit section</div>
+    </div>
+
+  </div>
+  );
+};
+
+export default page;
