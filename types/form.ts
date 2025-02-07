577cdb9fcd438507be7dca4ee606dfe7b13a9dd6
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-02-07 21:55:45 +0530

    create seperate typesfor each component

--- types/form.ts
+++ types/form.ts
@@ -1,8 +1,19 @@
-type Form ={
-    id:number;
-    ownerId:string;
-    published:boolean;
-    content:any;
-    submissions:number;
-    shareUrl:string
-}
+export  type Form = {
+  id: number;
+  ownerId: string;
+  published: boolean;
+  content: any;
+  submissions: number;
+  shareUrl: string;
+};
+
+export type Fields = {
+  name?: string;
+  label?: string;
+  placeholder?: string;
+};
+
+export type Content = {
+  formTitle: string;
+  formFields: Fields[];
+};
