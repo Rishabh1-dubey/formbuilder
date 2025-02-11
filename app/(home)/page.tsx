42ca82f5d25a2640b8c0f94f01e02b1852c5ab1b
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-02-12 00:32:10 +0530

    bug fix

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,14 +1,22 @@
 
+import { getForm } from '@/actions/getForm'
+import { getUserSubscription } from '@/actions/getUserSubscription'
 import HeroSection from '@/components/HeroSection'
 import PricingCard from '@/components/PricingCard'
 import { currentUser } from '@clerk/nextjs/server'
+import { string } from 'zod'
 const HomePage = async() => {
 
   const user = await currentUser()
+const forms = await getForm()
+const totalNumberOfFormCreated = forms?.data?.length || 0 as number;
+const isSubscribed = await getUserSubscription(user?.id as string) as boolean
+
+
 
   return (
     <div className='grid items-center justify-items-center min-h-screen p-8 gap-16 sm:p-28'>
-      <HeroSection/>
+      <HeroSection totalForms ={totalNumberOfFormCreated} isSubscribed ={isSubscribed}/>
       <PricingCard userId={user?.id}/>
     </div>
   )
