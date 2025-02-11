12feb3202094fb33d7b6cb2dc9405424add2d00d
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-02-11 22:10:41 +0530

    adding formdata

---
+++ app/forms/[formId]/layout.tsx
@@ -0,0 +1,9 @@
+
+ const layout = ({children}:{children:React.ReactNode}) => {
+  return (
+    <div className='bg-gray-400 dark:bg-black shadow-xl'>
+        {children}
+    </div>
+  )
+}
+ export default layout
---
+++ app/forms/[formId]/page.tsx
@@ -0,0 +1,33 @@
+import AiGenratedform from '@/components/AiGenratedform';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import prisma from '@/lib/prisma';
+import React from 'react'
+
+
+const SubmitForm = async ({params,}: {params: Promise<{ formId: string }>}) => {
+
+
+        const formId = (await params).formId;
+        if (!formId) {
+          return <h1>NO form Id found for id {formId}</h1>;
+        }
+        const form: any = await prisma.form.findUnique({
+          where: {
+            id: Number(formId),
+          },
+        });
+  return (
+    <Card className='max-w-xl mx-auto py-10'>
+    <CardHeader>
+      <CardTitle>
+        <h1 className="text-2xl text-center">{form.content?.formTitle}</h1>
+      </CardTitle>
+    </CardHeader>
+    <CardContent>
+      <AiGenratedform form={form} isEditMode={false} />
+    </CardContent>
+  </Card>
+  )
+}
+
+export default SubmitForm;
