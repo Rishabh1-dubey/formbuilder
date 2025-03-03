179b3d288095073dd0d5df657cbcb8b6ab3e4d3e
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-03-04 02:32:16 +0530

    adding stripe auth

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,23 +1,26 @@
-
-import { getForm } from '@/actions/getForm'
+import { getForms } from '@/actions/getForm'
 import { getUserSubscription } from '@/actions/getUserSubscription'
 import HeroSection from '@/components/HeroSection'
-import PricingCard from '@/components/PricingCard'
+import PricingPage from '@/components/PricingCard'
 import { currentUser } from '@clerk/nextjs/server'
-import { string } from 'zod'
-const HomePage = async() => {
+import { redirect } from 'next/navigation'
+import React from 'react'
 
-  const user = await currentUser()
-const forms = await getForm()
-const totalNumberOfFormCreated = forms?.data?.length || 0 as number;
-const isSubscribed = await getUserSubscription(user?.id as string) as boolean
-
-
+const HomePage =  async () => {
+  const user = await currentUser();
+  if(!user){
+    redirect("/sign-in");
+  }
+   
+  const forms = await getForms();
+  const totalNumberOfFormCreated = forms?.data?.length || 0 as number;
+  const isSubscribed = await getUserSubscription(user?.id as string) as boolean;
 
   return (
-    <div className='grid items-center justify-items-center min-h-screen p-8 gap-16 sm:p-28'>
-      <HeroSection totalForms ={totalNumberOfFormCreated} isSubscribed ={isSubscribed}/>
-      <PricingCard userId={user?.id}/>
+    <div className='grid items-center justify-items-center min-h-screen p-8 gap-16 sm:p-20'>
+        <HeroSection totalForms={totalNumberOfFormCreated} isSubscribed={isSubscribed}/>
+        <PricingPage userId = {user?.id}/>
+ 
     </div>
   )
 }
