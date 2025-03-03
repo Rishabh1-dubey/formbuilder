fe9b02b24a3c810cdad9ca6ba6d09e860b2b79ae
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-03-04 02:31:06 +0530

    adding max free token

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,8 @@
-import { PrismaClient } from "@prisma/client"
+
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-
 
  export const MAX_FREE_FORM_TOKEN = 3
