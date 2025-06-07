307af44f063eab4cbefe5b55e12c3e149cb5e98b
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-06-07 14:02:15 +0530

    change router

---
+++ app/home/layout.tsx
@@ -0,0 +1,28 @@
+"use client"
+import {DarkMode} from "@/components/DarkMode";
+import { Button } from "@/components/ui/button";
+import Logo from "@/components/ui/Logo";
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div >
+      <div className="border border-b">
+        <nav className="flex justify-between items-center max-w-7xl mx-auto py-6">
+          <Logo />
+          <div className="flex items-center gap-4">
+          <Link href="/dashboard/analytics">Home
+            <Button variant={"link"}>Dashboard</Button>
+          </Link>
+            <UserButton  afterSignOutUrl="/"/>
+            <DarkMode/>
+          </div>
+        </nav>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default layout;
