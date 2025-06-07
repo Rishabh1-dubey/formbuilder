49f74ca9f5b8d217d264e6a0ecb890933c7359d6
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-06-07 14:01:57 +0530

    clerk bug fix

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,16 +1,15 @@
- "use client"
- import { SignIn, useUser } from "@clerk/nextjs"
+"use client"
+import { SignIn, useUser } from "@clerk/nextjs"
 const Page = () => {
 
 
-  const {user} = useUser()
-  if (!user) {
-    return (
-      <div className="flex items-center justify-center mt-24">
-        <SignIn />
-      </div>
-    );
-  };
+
+  return (
+    <div className="flex items-center justify-center mt-24">
+      <SignIn />
+    </div>
+  );
+
 
 }
 export default Page
